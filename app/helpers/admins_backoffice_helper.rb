d84baa2233ec5ef3896cebdc640998de809d378b
thiago linch <thglinchin18@gmail.com>
2020-11-04 14:49:21 -0400

    aula 63 do módulo 05

--- app/helpers/admins_backoffice_helper.rb
+++ app/helpers/admins_backoffice_helper.rb
@@ -1,2 +1,8 @@
 module AdminsBackofficeHelper
+
+  def translate_field(object = nil, method = nil)
+    (object && method) ? object.model.human_attribute_name(method) :
+      "Informe os parâmetros corretamente"
+  end
+  
 end
