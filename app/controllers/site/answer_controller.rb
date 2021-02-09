f6ded8e1f99d11063d92faa0b4501dee9aa6d553
Thiago Linch <thglinchin18@gmail.com>
2021-02-09 19:29:41 -0400

    Refatorando o UserStatistic: alteração no Model do UserStatistic | Alteração no controller Site::Answer : na def question

--- app/controllers/site/answer_controller.rb
+++ app/controllers/site/answer_controller.rb
@@ -1,15 +1,8 @@
 class Site::AnswerController < SiteController
     def question
         @answer = Answer.find(params[:answer_id])
-        set_statistic(@answer)
+        UserStatistic.set_statistic(@answer, current_user)
     end
 
     private
-    def set_statistic(answer)
-        if user_signed_in?
-            user_statistic = UserStatistic.find_or_create_by(user: current_user)
-            answer.correct? ? user_statistic.right_questions += 1 : user_statistic.wrong_questions += 1
-            user_statistic.save
-        end
-    end
 end
