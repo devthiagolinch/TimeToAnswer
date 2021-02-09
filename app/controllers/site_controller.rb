70bd13f7bfd6eba3a48dd733d6720098afaa514b
Thiago Linch <thglinchin18@gmail.com>
2021-02-09 11:53:10 -0400

    forçando o user a se registrar/logar nos site

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,3 +1,12 @@
 class SiteController < ApplicationController
+    before_action :check_pagination
     layout 'site'
+
+    private
+    
+    def check_pagination
+        unless user_signed_in?
+            params.extract! (:page)
+        end
+    end
 end
